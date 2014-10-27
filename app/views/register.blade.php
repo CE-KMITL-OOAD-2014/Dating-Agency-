ad166f13c9e68cf12ba04c7ce0cd2aa9f5fb4905
bowtm <bow_tikham@hotmail.com>
2014-10-27 18:10:28 +0700

    sign up here update

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -50,7 +50,7 @@
                 <div class="col-md-12 control">
                     <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
                         Don't have an account! 
-                        <a href="http://localhost/laravel/public/buildprofile" onClick="$('#loginbox').hide(); $('#signupbox').show()">
+                        <a href="buildprofile" onClick="$('#loginbox').hide(); $('#signupbox').show()">
                             Sign Up Here
                         </a>
                     </div>
